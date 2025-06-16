b1220a649b78ed043803c61894c8570586b76d5e
Katrin Tomanek <katrin.tomanek@gmx.de>
2025-06-16 09:31:44 -0700

    Update run_es.sh

--- examples/language_teacher/run_es.sh
+++ examples/language_teacher/run_es.sh
@@ -3,7 +3,7 @@ python ../../voice_agent_ui.py \
    --tts_model_path es_ES-carlfm-x_low.onnx  \
    --asr_model_name whisper_tiny \
    --language es \
-   --system_prompt "`cat examples/language_teacher/instructions_es.txt `" \
+   --system_prompt "`cat instructions_es.txt `" \
    --start_message "Buenas dias! Soy tu maestra de español y estoy súper emocionada de aprender contigo hoy. ¿Qué quieres practicar?" \
    --min_partial_duration 0.5 \
    --end_of_utterance_duration 0.75 \
