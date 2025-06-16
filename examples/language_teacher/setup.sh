8372f835a43fca2c3b1fa25d3cb11213b8364515
Katrin Tomanek <katrin.tomanek@gmx.de>
2025-06-16 08:30:35 -0700

    better german TTS model

--- examples/language_teacher/setup.sh
+++ examples/language_teacher/setup.sh
@@ -7,8 +7,8 @@ ollama pull llama3.2:1b
 # many of the other voices here produce very poor audio, hallucinate or mumble considerably.
 
 # DE
-wget https://huggingface.co/rhasspy/piper-voices/blob/main/de/de_DE/thorsten/low/de_DE-thorsten-low.onnx
-wget https://huggingface.co/rhasspy/piper-voices/blob/main/de/de_DE/thorsten/low/de_DE-thorsten-low.onnx.json
+wget https://huggingface.co/rhasspy/piper-voices/blob/main/de/de_DE/thorsten/low/de_DE-karlsson-low.onnx
+wget https://huggingface.co/rhasspy/piper-voices/blob/main/de/de_DE/thorsten/low/de_DE-karlsson-low.onnx.json
 
 # ES
 wget https://huggingface.co/rhasspy/piper-voices/blob/main/es/es_ES/carlfm/x_low/es_ES-carlfm-x_low.onnx
